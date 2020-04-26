749a3f4c33edc91ce25726938cd26a50c209840f
keanh <keanh275@gmail.com>
2020-04-26 13:58:38 +0700

    add

---
+++ dictionary/out/artifacts/dictionary_war_exploded/WEB-INF/views/dictionary.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Admin
+  Date: 26/04/2020
+  Time: 1:47 CH
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Dictionary</title>
+</head>
+<body>
+<h1>Từ cần dịch là: ${word}</h1>
+</body>
+</html>
---
+++ dictionary/src/controllers/dictionary.java
@@ -0,0 +1,24 @@
+package controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+public class dictionary {
+    @GetMapping("/dictionary")
+    public String dictionary(@RequestParam String word, Model model){
+        Map<String,String> dictionary = new HashMap<>();
+        dictionary.put("hello","Xin chào");
+        dictionary.put("how","Thế nào");
+        dictionary.put("book","Quyển vở");
+        dictionary.put("computer","Máy tính");
+        String result = dictionary.get(word);
+        model.addAttribute("word",result);
+        return "dictionary";
+    }
+}
